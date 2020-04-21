898c777f04c3d6191d5276bf441e910168281554
user <user@DESKTOP-GFC4K3O>
2020-04-21 12:09:32 +0530

    commit

---
+++ DemoBootSpring/src/main/java/com/example/demo/DemoBootSpringApplication.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoBootSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoBootSpringApplication.class, args);
+		System.out.println("welcome to CVSR");
+	}
+
+}
